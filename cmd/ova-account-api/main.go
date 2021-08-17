da273c9e6afdff020124500f7338a6eb66fdfebc
Sergey A <sergey.v.akkuzin@gmail.com>
2021-08-17 14:58:24 +0300

    Add the app configuration with the update in the loop

--- cmd/ova-account-api/main.go
+++ cmd/ova-account-api/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+
+	"github.com/ozonva/ova-account-api/internal/app"
 )
 
 func main() {
-	fmt.Println("Service ova-account-api.")
+	confPath := flag.String("conf", "./configs/app.json", "Location of the configuration file")
+	flag.Parse()
+
+	config, err := app.ParseConfig(*confPath)
+	if err != nil {
+		log.Fatal("Can't process the configuration: ", err)
+	}
+
+	fmt.Printf("Service %s.\n", config.Name)
+
+	updatingConfiguration(config, *confPath)
+}
+
+func updatingConfiguration(config *app.Config, path string) {
+	times := 10
+	for i := 0; i < times; i++ {
+		err := config.Update(path)
+		if err != nil {
+			log.Println("Unable to update the configuration:", err)
+			continue
+		}
+
+		log.Println("Successfully updated the configuration")
+	}
 }
---
+++ internal/app/config.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+// Config represents the application configuration.
+type Config struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Version string `json:"version"`
+}
+
+// NewConfig creates a new Config.
+func NewConfig() *Config {
+	return &Config{
+		Name:    "ova-account-api",
+		Address: ":8080",
+		Version: "v1",
+	}
+}
+
+// ParseConfig parses the configuration from the file at the specified path.
+func ParseConfig(path string) (*Config, error) {
+	conf := NewConfig()
+	err := conf.Update(path)
+
+	return conf, err
+}
+
+// Update updates the configuration from the file at the specified path.
+func (c *Config) Update(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, c)
+}

65043d972fee348a59cef14326a74c62db64fac3
Sergey A <sergey.v.akkuzin@gmail.com>
2021-09-07 13:08:42 +0300

    refactor: use the global logger

--- cmd/ova-account-api/main.go
+++ cmd/ova-account-api/main.go
@@ -10,67 +10,70 @@ import (
 	"github.com/ozonva/ova-account-api/internal/app"
 	"github.com/ozonva/ova-account-api/internal/metrics"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	flag "github.com/spf13/pflag"
 )
 
 func main() {
-	confPath := flag.StringP("conf", "c", ".env", "Location of the configuration file")
-	debug := flag.Bool("debug", false, "sets log level to debug")
+	confPath := flag.StringP("conf", "c", ".env", "location of the configuration file")
+	_ = flag.Bool("debug", false, "sets log level to debug")
 	flag.Parse()
-
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	if *debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	}
-	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
 
 	application, err := app.Init(*confPath)
 	if err != nil {
-		logger.Fatal().Err(err).Msg("")
+		log.Fatal().Err(err).Send()
 	}
 	defer func() {
 		if err := application.Release(); err != nil {
-			logger.Error().Err(err)
+			log.Error().Err(err).Msg("Got an error while releasing app resources.")
 		}
 	}()
 
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if application.Conf.Debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
 	fmt.Printf("Service %s.\n", application.Conf.Name)
 
-	updatingConfiguration(logger, application.Conf, 10)
+	updatingConfiguration(application.Conf, 10)
 
-	logger.Info().Msg("Starting the servers...")
-	server := api.NewServer(logger, application)
+	log.Info().Msg("Starting the servers...")
+	server := api.NewServer(application)
 	server.Start()
 
 	metricsServer := metrics.NewServer()
 	metricsServer.Start()
-	logger.Info().Msgf("The application is ready to serve requests on port %s.", application.Conf.GrpcPort)
+	log.Info().Msgf("The application is ready to serve requests on port %s.", application.Conf.GrpcPort)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
 	select {
 	case x := <-interrupt:
-		logger.Info().Msgf("Received a signal: %v", x)
+		log.Info().Msgf("Received a signal: %v", x)
 	case err := <-server.Notify():
-		logger.Error().Err(err).Msg("Received an error from the grpc server")
+		log.Error().Err(err).Msg("Received an error from the grpc server")
 	case err := <-metricsServer.Notify():
-		logger.Error().Err(err).Msg("Received an error from the metrics server")
+		log.Error().Err(err).Msg("Received an error from the metrics server")
 	}
 
-	logger.Info().Msg("Stopping the servers...")
+	log.Info().Msg("Stopping the servers...")
 	server.Stop()
-	metricsServer.Stop()
+
+	if err := metricsServer.Stop(); err != nil {
+		log.Error().Err(err).Msg("Got an error while stopping the metrics server.")
+	}
 }
 
-func updatingConfiguration(logger zerolog.Logger, config *app.Config, times int) {
+func updatingConfiguration(config *app.Config, times int) {
 	for i := 0; i < times; i++ {
 		err := config.Update("./configs/app.json")
 		if err != nil {
-			logger.Err(err).Msg("Unable to update the configuration")
+			log.Err(err).Msg("Unable to update the configuration")
 			continue
 		}
 
-		logger.Info().Msg("Successfully updated the configuration")
+		log.Info().Msg("Successfully updated the configuration")
 	}
 }
